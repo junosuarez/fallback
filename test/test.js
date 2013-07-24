80b6c421159d556737925dd7d21eb749e5aa869c
Nathan Peck <nathan@storydesk.com>
2013-07-24 07:39:18 -0400

    Adding a test for the case where none of the items in the fallback sequence succeed.

--- test/test.js
+++ test/test.js
@@ -15,9 +15,6 @@ test('fallback tries alternatives in serial', function (t) {
     t.equal(iterations, 2)
     t.end()
   })
-
-
-
 })
 
 test('if an error is returned the sequence is interrupted', function (t) {
@@ -33,8 +30,6 @@ test('if an error is returned the sequence is interrupted', function (t) {
     t.equal(iterations, 1)
     t.end()
   })
-
-
 })
 
 test('the fallback sequence ends on the first successful item', function (t) {
@@ -53,6 +48,25 @@ test('the fallback sequence ends on the first successful item', function (t) {
   }, function (err) {
     t.equal(iterations, 3)
     t.equal(tried.indexOf('albania'), -1)
+    t.end()
+  })
+})
+
+test('if no items in the fallback sequence succeed it returns false', function (t) {
+  t.plan(2)
+
+  var iterations = 0
+  var tried = []
+
+  fallback([1,3,4,5], function (item, cb) {
+    iterations++
+    if (item == 2) {
+      return cb(null, item)
+    }
+    cb()
+  }, function (err, result) {
+    t.equal(iterations, 4)
+    t.equal(result, false)
     t.end()
   })
 })
